c308e236b92550d40d760947b2072ab030035fb5
maklumi <maklumi@hotmail.com>
2016-12-26 18:25:00 +0800

    add contact and contactdata

---
+++ src/sample/Contact.java
@@ -0,0 +1,73 @@
+package sample;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * Created by HomePC on 26/12/2016.
+ */
+public class Contact {
+    public SimpleStringProperty firstName = new SimpleStringProperty("");
+    public SimpleStringProperty lastName = new SimpleStringProperty("");
+    public SimpleStringProperty phoneNumber = new SimpleStringProperty("");
+    public SimpleStringProperty notes = new SimpleStringProperty("");
+
+    public Contact() {
+    }
+
+    ;
+
+    public Contact(String firstName, String lastName, String phoneNumber, String notes) {
+        this.firstName.set(firstName);
+        this.lastName.set(lastName);
+        this.phoneNumber.set(phoneNumber);
+        this.notes.set(notes);
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public SimpleStringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public SimpleStringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber.get();
+    }
+
+    public SimpleStringProperty phoneNumberProperty() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber.set(phoneNumber);
+    }
+
+    public String getNotes() {
+        return notes.get();
+    }
+
+    public SimpleStringProperty notesProperty() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes.set(notes);
+    }
+}
